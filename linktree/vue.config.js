17f04a94d9b7fc9a98e2239612468b43948dc38e
gwni0214 <sbxjs6999@naver.com>
2023-05-14 11:14:03 +0900

    asd

--- linktree/vue.config.js
+++ linktree/vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath : '/linktree',
+  publicPath : '/gwniLinkTree',
   outputDir : 'docs',
 })
